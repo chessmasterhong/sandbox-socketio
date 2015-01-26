bbf2ba5b51550915f6e0d9e3d9282cbdd506e50a
Kevin Chan <chessmasterhong@yahoo.com>
2015-01-25 19:17:25 -0500

    Fixed JSHint warning with creating functions in loop. Fixed undefined variable in textFn(). Added explicit return variable to testFn(). Moved engine core to window.game namespace. Updated .jshintrc.

--- public/scripts/core.js
+++ public/scripts/core.js
@@ -1,6 +1,8 @@
 (function() {
     'use strict';
 
+    window.game = {};
+
     /**
      * Simple JavaScript Inheritance for ECMAScript 5.1 based on John Resig's code.
      * Inspired by base2 and Prototype.
@@ -8,17 +10,35 @@
      * @see http://ejohn.org/blog/simple-javascript-inheritance/
      * @see http://stackoverflow.com/questions/15050816/is-john-resigs-javascript-inheritance-snippet-deprecated
      */
-    var fnTest = /xyz/.test(function() { xyz; }) ? /\bparent\b/ : /.*/;
+    var testFn = /xyz/.test(function() {
+        var xyz;
+        return xyz;
+    }) ? /\bparent\b/ : /.*/;
 
     // The base Class implementation (does nothing)
-    window.Class = function() {};
+    window.game.Class = function() {};
 
     // Create a new Class that inherits from this class
-    window.Class.extend = function(props) {
+    window.game.Class.extend = function(props) {
         var parent = this.prototype;
 
         // Instantiate a base class (but only create the instance, don't run the init constructor)
         var proto = Object.create(parent);
+
+        var makeFn = function(name, fn) {
+            return function() {
+                var tmp = this.parent;
+
+                // Add a new .parent() method that is the same method but on the super-class
+                this.parent = parent[name];
+
+                // The method only need to be bound temporarily, so we remove it when we're done executing
+                var ret = fn.apply(this, arguments);
+                this.parent = tmp;
+
+                return ret;
+            };
+        };
 
         // Copy the properties over onto the new prototype
         for(var name in props) {
@@ -26,22 +46,9 @@
             proto[name] = (
                 typeof props[name] === 'function' &&
                 typeof parent[name] === 'function' &&
-                fnTest.test(props[name])
+                testFn.test(props[name])
             ) ?
-                (function(name, fn) {
-                    return function() {
-                        var tmp = this.parent;
-
-                        // Add a new .parent() method that is the same method but on the super-class
-                        this.parent = parent[name];
-
-                        // The method only need to be bound temporarily, so we remove it when we're done executing
-                        var ret = fn.apply(this, arguments);
-                        this.parent = tmp;
-
-                        return ret;
-                    };
-                })(name, props[name]) :
+                makeFn(name, props[name]) :
                 proto[name] = props[name];
         }
 
@@ -60,7 +67,7 @@
         Class.prototype.constructor = Class;
 
         // And make this class extendable
-        Class.extend = window.Class.extend;
+        Class.extend = window.game.Class.extend;
 
         return Class;
     };
